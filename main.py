af02829d0643871e8d3e3848e97d4b82f38cb56f
edu-servettazt <edu-servettazt@eduge.ch>
2020-12-07 10:53:24 +0100

    ajout fichier de base

---
+++ main.py
@@ -0,0 +1,12 @@
+#votre nom votre prenom
+
+if __name__ == '__main__':
+    nom_groupe = "Stade Genève - adultes I"
+    annee_en_cours = 2020
+    names = ['Bob', 'Jane', 'Jack', 'Chloe', 'Mat', 'Beth', 'Xiao']
+    ages = [20, 22, 23, 18, 35, 40, 39]
+    is_male = [True, False, True, False, True, False, False]
+    chrono_manche_1 = [13.4, 15.2, 12.8, 12.7, 12.8, 13.3, 13.9]
+    chrono_manche_2 = [13.6, 14.2, 12.7, 12.5, 14.7, 13.4, 13.8]
+    chrono_manche_3 = [14.5, 14.1, 12.8, 13.7, 13.4, 13.5, 12.9]
+    resultats = [chrono_manche_1, chrono_manche_2, chrono_manche_3]
