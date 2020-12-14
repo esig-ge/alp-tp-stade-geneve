f331fb80dcf10855e193732063a0aea8e2323fe9
Thomas Servettaz <tservettaz@metavision.ch>
2020-12-14 12:05:11 +0100

    corrigé exercice 3

--- main.py
+++ main.py
@@ -12,6 +12,33 @@ def en_tete_mieux(nb_etoiles, titre):
     print("*" * (nb_etoiles + len(titre) + 2 + nb_etoiles))
 
 
+def affiche_categ():
+    nom = input('Quel est votre nom? : ')
+    genre = input('Quel est votre genre? (h pour homme, f pour femme) : ')
+    age = int(input('Quel est votre âge? : '))
+    temps = float(input('Quel est votre meilleur temps sur 100m? : '))
+    categorie = '?'
+
+    if genre == 'f':
+        if age < 18:
+            categorie = 'FEMME J'
+        elif temps <= 15.0:
+            categorie = 'FEMME 1'
+        else:
+            categorie = 'FEMME 2'
+    elif genre == 'h':
+        if age < 18:
+            categorie = 'HOMME J'
+        elif temps < 14.5:
+            categorie = 'HOMME 1'
+        elif temps <= 16.0:
+            categorie = 'HOMME 2'
+        else:
+            categorie = 'HOMME 3'
+
+    print('votre categorie est: {}'.format(categorie))
+
+
 if __name__ == '__main__':
     nom_groupe = "Stade Genève - adultes I"
     annee_en_cours = 2020
@@ -30,3 +57,8 @@ if __name__ == '__main__':
     en_tete_mieux(4, "ALP")
     en_tete_mieux(10, "Thomas Servettaz")
     en_tete_mieux(1, "ESIG")
+
+    #exercice 3
+    affiche_categ()
+    affiche_categ()
+    affiche_categ()
